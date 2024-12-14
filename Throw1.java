5d22f4ee63b4f4ce392db2ea9cd953cb177698fe
Hemasree837 <hemasree837@gmail.com>
2024-12-14 15:16:37 +0530

    Add files via upload

---
+++ Throw1.java
@@ -0,0 +1,12 @@
+public class Throw1 {
+    public static void main(String args[])
+    {
+        try{
+            throw new Exception("Error");
+        }
+        catch(Exception e)
+        {
+            System.out.println(e.getMessage());
+        }
+    }
+}
