0d3e080d35c0987dfa7acf2f213f35c24aff2d5b
Hemasree837 <hemasree837@gmail.com>
2024-12-14 15:16:50 +0530

    Add files via upload

---
+++ Throw2.java
@@ -0,0 +1,22 @@
+import java.util.*;
+public class Throw2 {
+    public static void a(int a,int b)
+    {
+        if(b==0)
+        {
+            throw new ArithmeticException("Cannot Divide");
+        }
+        System.out.println(a/b);
+    }
+    public static void main(String args[])
+    {
+        try
+        {
+            a(10,0);
+        }
+        catch(ArithmeticException e)
+        {
+            System.out.println(e.getMessage());
+        }
+    }
+}
